a793c1caaac9a265b2c6dc87f0abedd46800b273
mredfern79 <mark@theredfernfamily.co.uk>
2016-12-09 14:13:16 +0000

    added config information to config.php and database.php

--- application/config/database.php
+++ application/config/database.php
@@ -74,11 +74,11 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> '8889',
 	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'SRMS_P',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
