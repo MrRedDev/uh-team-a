697b264bdc4029b067b77f5ff85187437ad1f4bd
Jakub Bachniak <jj.bachniak@gmail.com>
2016-12-18 21:11:26 +0000

    Added custom sql view 'therapy popularity'

--- application/controllers/Therapy.php
+++ application/controllers/Therapy.php
@@ -26,6 +26,18 @@ class Therapy extends CI_Controller {
         $this->load->view('includes/template', $data);
     }
 
+    public function therapy_popularity_output($output = null)
+    {
+        $this->load->helper('form');
+
+        $data['output'] = $output;
+        $data['main_content'] = 'site/therapy_popularity_view';
+        $data['user'] = $this->session->userdata('username');
+        $data['al'] = $this->session->userdata('al');
+        $this->load->view('includes/template', $data);
+    }
+
+
     public function therapy()
     {
 
@@ -121,6 +133,23 @@ class Therapy extends CI_Controller {
         $output = $crud->render();
 
         $this->therapy_output($output);
+    }
+
+    public function therapy_popularity()
+    {
+        $crud = new grocery_CRUD();
+        $crud->set_theme('flexigrid');
+
+        //table name exact from database
+        $crud->set_table('therapypopularity');
+        $crud->set_primary_key('Therapy');
+        //give focus on name used for operations e.g. Add Order, Delete Order
+        $crud->set_subject('Popularity');
+        $crud->unset_operations();
+
+        $output = $crud->render();
+
+        $this->therapy_popularity_output($output);
     }
 
 
